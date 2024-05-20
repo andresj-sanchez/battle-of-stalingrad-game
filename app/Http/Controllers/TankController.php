c8bb245038c90ece79d43cb424c64310a12dea6f
Andres Sanchez <55501523+andresj-sanchez@users.noreply.github.com>
2024-05-20 00:17:18 -0400

    Add models, resource controllers, and migrations

---
+++ app/Http/Controllers/TankController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tank;
+use Illuminate\Http\Request;
+
+class TankController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tank $tank)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tank $tank)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tank $tank)
+    {
+        //
+    }
+}

a0d94bdcc9edee613204981d97ccb3705208440c
Andres Sanchez <55501523+andresj-sanchez@users.noreply.github.com>
2024-05-27 00:11:31 -0400

    Fix: Docker deployment

--- docker/couchbase/init.sh
+++ docker/couchbase/init.sh
@@ -17,19 +17,19 @@ wait_for_couchbase() {
 wait_for_couchbase
 
 # Setup cluster
-couchbase-cli cluster-init -c 127.0.0.1:8091 \
-    --cluster-name battleofstalingrad \
-    --cluster-username Administrator \
-    --cluster-password password \
+couchbase-cli cluster-init -c $COUCHBASE_HOST \
+    --cluster-name $COUCHBASE_CLUSTER \
+    --cluster-username $COUCHBASE_ADMINISTRATOR_USERNAME \
+    --cluster-password $COUCHBASE_ADMINISTRATOR_PASSWORD \
     --services data,index,query \
     --cluster-ramsize 1024 \
     --cluster-index-ramsize 512
 
 # Create bucket
-couchbase-cli bucket-create -c 127.0.0.1:8091 \
-    --bucket battleofstalingrad \
-    --username Administrator \
-    --password password \
+couchbase-cli bucket-create -c $COUCHBASE_HOST \
+    --bucket $COUCHBASE_BUCKET \
+    --username $COUCHBASE_USERNAME \
+    --password $COUCHBASE_PASSWORD \
     --bucket-type couchbase \
     --bucket-ramsize 256
 
